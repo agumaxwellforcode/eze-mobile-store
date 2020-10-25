76ab257bc62424c8ce5de1ea4d92a387186f9bde
Salami Bashir <bashirsalami78@gmail.com>
2020-10-25 12:50:16 +0100

    Disregard this

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,17 +4,17 @@ import '../css/filter.css'
 
 export default function Filter() {
     return (
-        <div classNameName='filter-category'>
+        <div className='filter-category'>
            <div className ="title">
                <h2>category</h2>
            </div>
            <div id="myBtn">
-                <button className="btn-text active" onclick="filterSelection('all')"> All</button>
-                <button className="btn-text" onclick="filterSelection('cars')"> Iphone</button>
-                <button className="btn-text" onclick="filterSelection('Samsung')"> Samsung</button>
-                <button className="btn-text" onclick="filterSelection('Ipad')"> Ipad</button>
-                <button className="btn-text" onclick="filterSelection(' MacBook')"> MacBook</button>
-                <button className="btn-text" onclick="filterSelection(' Airpods')"> Airpods</button>
+                <button className="btn-text active" onClick="filterSelection('all')"> All</button>
+                <button className="btn-text" onClick="filterSelection('cars')"> Iphone</button>
+                <button className="btn-text" onClick="filterSelection('Samsung')"> Samsung</button>
+                <button className="btn-text" onClick="filterSelection('Ipad')"> Ipad</button>
+                <button className="btn-text" onClick="filterSelection(' MacBook')"> MacBook</button>
+                <button className="btn-text" onClick="filterSelection(' Airpods')"> Airpods</button>
            </div>
         </div>
     )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../css/Navbar.css";
+
+export default class Navbar extends Component {
+  state = {
+    search_value: "",
+  };
+  render() {
+    return (
+      <div className="header-container">
+        <nav>
+          <header className="header-text">
+            <h1>Shop our latest available stock here</h1>
+          </header>
+          <div>
+            {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
+
+            <form className="form-inline ml-auto">
+              <div className="md-form my-0">
+                <input
+                  className="form-control"
+                  type="text"
+                  value={this.state.search_value}
+                  onChange={this.handleChange.bind(this)}
+                  placeholder="Enter search term (e.g iphone x, 128Gb or A1)"
+                  aria-label="Search"
+                ></input>
+              </div>
+              <button
+                className="btn btn-outline-white btn-md my-0 ml-sm-2 btnsearch"
+                type="submit"
+              >
+                {" "}
+                Search <span></span>{" "}
+              </button>
+            </form>
+
+            {/* </div> */}
+          </div>
+        </nav>
+        <img src="" alt="" />
+      </div>
+    );
+  }
+  handleChange(e) {
+    this.setState({ search_value: e.target.value });
+  }
+}
